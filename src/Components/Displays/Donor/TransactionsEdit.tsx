974fcfdd06e6285349ca43234204a415db768bd5
Connor Kalahiki <connorkalahiki@gmail.com>
2021-04-10 14:26:42 -0700

    one bug left then pau

--- src/Components/Displays/Donor/TransactionsEdit.tsx
+++ src/Components/Displays/Donor/TransactionsEdit.tsx
@@ -14,10 +14,7 @@ interface IProps{
 }
 
 interface IState{
-    description: string;
     amount: number;
-    endDate: '';
-    title: string;
 }
 
 interface ITransaction {
@@ -30,21 +27,18 @@ export default class TransactionsEdit extends React.Component<IProps, IState>{
     constructor(props: IProps){
         super(props);
         this.state = {
-            description: '',
-            amount: NaN,
-            endDate: '',
-            title: ''
+            amount: NaN
         }
         this.inputCompiler = this.inputCompiler.bind(this);
-        this.campaignUpdate = this.campaignUpdate.bind(this);
+        this.transactionUpdate = this.transactionUpdate.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    campaignUpdate(e: React.SyntheticEvent){
+    transactionUpdate(e: React.SyntheticEvent){
         e.preventDefault();
-        fetch(`${this.props.baseURL}/campaigns/${this.props.transactionToUpdate.id}`, {
+        fetch(`${this.props.baseURL}/transactions/${this.props.transactionToUpdate.id}`, {
             method: 'PUT',
-            body: JSON.stringify({campaign: {title: this.state.title, description: this.state.description, amount: this.state.amount, endDate: this.state.endDate}}),
+            body: JSON.stringify({transaction: {amount: this.state.amount}}),
             headers: new Headers({
                 'Content-Type': 'application/json',
                 'Authorization': this.props.sessionToken
@@ -53,7 +47,7 @@ export default class TransactionsEdit extends React.Component<IProps, IState>{
         .then((res) => {
             this.props.fetchTransactions();
             this.props.updateOff();
-            alert('Campaign successfully updated');
+            alert('Transaction successfully updated');
         })
     }
 
@@ -64,25 +58,16 @@ export default class TransactionsEdit extends React.Component<IProps, IState>{
 
     handleSubmit(e: React.SyntheticEvent){
         e.preventDefault();
-        this.campaignUpdate(e);
+        this.transactionUpdate(e);
     }
 
     render(){
         return(
             <Dialog open={true}>
-                <DialogTitle style={{textAlign: 'center'}}>Edit Your Campaign</DialogTitle>
+                <DialogTitle style={{textAlign: 'center'}}>Edit Your Transaction</DialogTitle>
                 <form style={{padding: "2em", width: '35vw', textAlign: 'center'}}>
-                <FormGroup>
-                        <TextField variant='filled' value={this.state.title} name='title' label='Title' onChange={this.inputCompiler} type='text' id="standard-adornment-amount"></TextField>
-                    </FormGroup>
-                    <FormGroup>
-                        <TextField id='filled-multiline-static' multiline rows={5} label='Description' variant='filled' value={this.state.description} name='description' onChange={this.inputCompiler} type='text'></TextField>
-                    </FormGroup>
                     <FormGroup>
                         <TextField label='Amount' variant='filled' value={this.state.amount} name='amount' onChange={this.inputCompiler} type='number' id="standard-adornment-amount"></TextField>
-                    </FormGroup>
-                    <FormGroup>
-                        <TextField variant='filled' value={this.state.endDate} name='endDate' onChange={this.inputCompiler} type='date' id="standard-adornment-amount"></TextField>
                     </FormGroup>
                     <br/>
                     <Button variant='contained' style={{backgroundColor: '#E24E42', color:'white', borderRadius: '25px', marginRight:'2em'}} onClick={this.handleSubmit}>Update Transaction</Button>
